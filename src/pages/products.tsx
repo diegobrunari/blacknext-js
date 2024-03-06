e0062edd28c3737c63a548fdcf53626c2527ab97
Diego Brunari <diego.skp@hotmail.com>
2024-03-06 14:54:40 -0300

    new branch

---
+++ src/pages/products.tsx
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import type { NextPage } from 'next';
+
+const Products: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Nossos Produtos</title>
+        <meta name="description" content="Conheça todos os nossos produtos"/>
+        <link rel="icon" href="favicon.ico"  />
+      </Head>
+
+        <h1>
+            Nossos Produtos
+        </h1>
+
+    </>
+  )
+}
+
+export default Products
